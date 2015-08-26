e2ed0e1ca8e04360cb818a4e855dd45030646583
aaronbrackett472 <aaronbrackett472@gmail.com>
2015-08-25 22:21:36 -0400

    Using Requires
    Seperating the files out, restructuring champion-model.js to make it
    better, make getChamp works, exports functions etc etc

--- champion-model.js
+++ champion-model.js
@@ -1,114 +1,109 @@
-(function(window, document, undefined) {
-  var ChampionModel = {}; //this line has a semi-colon -tcj
+var api_key = 'b9f2effe-c90e-4633-b36b-5716684ccbac' //should these lines have semi-colons? -tcj
+var FREE_CHAMPS_URL= 'https://na.api.pvp.net/api/lol/na/v1.2/champion?freeToPlay=true&api_key=' + api_key //should these lines have semi-colons? -tcj
+var CHALLENGER_URL = 'https://na.api.pvp.net/api/lol/na/v2.5/league/challenger?type=RANKED_SOLO_5x5&api_key' + api_key //should these lines have semi-colons? -tcj
+var MH_URL_1 = 'https://na.api.pvp.net/api/lol/na/v2.2/matchhistory/'
+var MH_URL_2 = '?championIds=' //should these lines have semi-colons? -tcj
+var MH_URL_3 = '&api_key=' + api_key //should these lines have semi-colons? -tcj
+var ITEM_URL_1 = 'https://global.api.pvp.net/api/lol/static-data/na/v1.2/item/'
+var ITEM_URL_2 = '?itemData=image&api_key=' + api_key
+var CHAMP_URL_1 = 'https://global.api.pvp.net/api/lol/static-data/na/v1.2/champion/'
+var CHAMP_URL_2 = '?api_key=0fb0440c-84e2-41b1-8a11-2fc3ccc7a1ea'
+var STATUS_OK = 200;
 
-  var api_key = 'b9f2effe-c90e-4633-b36b-5716684ccbac' //should these lines have semi-colons? -tcj
-  var FREE_CHAMPS_URL= 'https://na.api.pvp.net/api/lol/na/v1.2/champion?freeToPlay=true&api_key=' + api_key //should these lines have semi-colons? -tcj
-  var CHALLENGER_URL = 'https://na.api.pvp.net/api/lol/na/v2.5/league/challenger?type=RANKED_SOLO_5x5&api_key' + api_key //should these lines have semi-colons? -tcj
-  var MH_URL_1 = 'https://na.api.pvp.net/api/lol/na/v2.2/matchhistory/'
-  var MH_URL_2 = '?championIds=' //should these lines have semi-colons? -tcj
-  var MH_URL_3 = '&api_key=' + api_key //should these lines have semi-colons? -tcj
-  var ITEM_URL_1 = 'https://global.api.pvp.net/api/lol/static-data/na/v1.2/item/'
-  var ITEM_URL_2 = '?itemData=image&api_key=' + api_key
-  var STATUS_OK = 200;
+exports.freeChamps = function(callback) {
+  var getRequest = new XMLHttpsRequest();
+  getRequest.addEventListener('load', function(event){
+    if (getRequest.status != STATUS_OK){
+      callback(getRequest.responseText);
+    }
+    else{
+      var JSONPosts = getRequest.responseText;
+      var parsedPosts = JSON.parse(JSONPosts);
+      return callback(null, parsedPosts);
+    }
+  });
+  getRequest.open('GET', FREE_CHAMPS_URL);
+  getRequest.send();
+},
 
+exports.challengers = function(callback) {
+  var getRequest = new XMLHttpsRequest();
+  getRequest.addEventListener('load', function(event){
+    if (getRequest.status != STATUS_OK){
+      callback(getRequest.responseText);
+    }
+    else{
+      var JSONPosts = getRequest.responseText;
+      var parsedPosts = JSON.parse(JSONPosts);
+      return callback(null, parsedPosts);
+    }
+  });
+  getRequest.open('GET', CHALLENGER_URL);
+  getRequest.send();
+},
 
-  ChampionModel.freeChamps = function(callback) {
-    var getRequest = new XMLHttpsRequest();
-    getRequest.addEventListener('load', function(event){
-      if (getRequest.status != STATUS_OK){
-        callback(getRequest.responseText);
-      }
-      else{
-        var JSONPosts = getRequest.responseText;
-        var parsedPosts = JSON.parse(JSONPosts);
-        return callback(null, parsedPosts);
-      }
-    });
-    getRequest.open('GET', FREE_CHAMPS_URL);
-    getRequest.send();
-  };
+exports.matchHistory = function(callback, summonerID, champID) {
+  var getRequest = new XMLHttpsRequest();
+  getRequest.addEventListener('load', function(event){
+    if (getRequest.status != STATUS_OK){
+      callback(getRequest.responseText);
+    }
+    else{
+      var JSONPosts = getRequest.responseText;
+      var parsedPosts = JSON.parse(JSONPosts);
+      return callback(null, parsedPosts);
+    }
+  });
+  getRequest.open('GET', MH_URL_1 + summonerID + MH_URL_2 + champID + MH_URL_3);
+  getRequest.send();
+},
 
-  ChampionModel.challengers = function(callback) {
-    var getRequest = new XMLHttpsRequest();
-    getRequest.addEventListener('load', function(event){
-      if (getRequest.status != STATUS_OK){
-        callback(getRequest.responseText);
-      }
-      else{
-        var JSONPosts = getRequest.responseText;
-        var parsedPosts = JSON.parse(JSONPosts);
-        return callback(null, parsedPosts);
-      }
-    });
-    getRequest.open('GET', CHALLENGER_URL);
-    getRequest.send();
-  };
+exports.getItem = function(callback, itemID){
+  var getRequest = new XMLHttpsRequest();
+  getRequest.addEventListener('load', function(event){
+    if (getRequest.status != STATUS_OK){
+      callback(getRequest.responseText);
+    }
+    else{
+      var JSONPosts = getRequest.responseText;
+      var parsedPosts = JSON.parse(JSONPosts);
+      return callback(null, parsedPosts);
+    }
+  });
+  getRequest.open('GET', ITEM_URL_1 + itemID + ITEM_URL_2);
+  getRequest.send();
+},
 
-  ChampionModel.matchHistory = function(callback, summonerID, champID) {
-    var getRequest = new XMLHttpsRequest();
-    getRequest.addEventListener('load', function(event){
-      if (getRequest.status != STATUS_OK){
-        callback(getRequest.responseText);
-      }
-      else{
-        var JSONPosts = getRequest.responseText;
-        var parsedPosts = JSON.parse(JSONPosts);
-        return callback(null, parsedPosts);
-      }
-    });
-    getRequest.open('GET', MH_URL_1 + summonerID + MH_URL_2 + champID + MH_URL_3);
-    getRequest.send();
-  };
+exports.getChamp = function(callback, champID){
+  var getRequest = new XMLHttpsRequest();
+  getRequest.addEventListener('load', function(event){
+    if (getRequest.status != STATUS_OK){
+      callback(getRequest.responseText);
+    }
+    else{
+      var JSONPosts = getRequest.responseText;
+      var parsedPosts = JSON.parse(JSONPosts);
+      return callback(null, parsedPosts);
+    }
+  });
+  getRequest.open('GET', CHAMP_URL_1 + champID + CHAMP_URL_2);
+  getRequest.send();
+},
 
-  ChampionModel.getItem = function(callback, itemID){
-    var getRequest = new XMLHttpsRequest();
-    getRequest.addEventListener('load', function(event){
-      if (getRequest.status != STATUS_OK){
-        callback(getRequest.responseText);
-      }
-      else{
-        var JSONPosts = getRequest.responseText;
-        var parsedPosts = JSON.parse(JSONPosts);
-        return callback(null, parsedPosts);
-      }
-    });
-    getRequest.open('GET', ITEM_URL_1 + itemID + ITEM_URL_2);
-    getRequest.send();
-  }
+exports.postFatJSON = function(post, callback) {
+  var postRequest = new XMLHttpRequest();
+  postRequest.addEventListener('load', function(event){
+    if (postRequest.status != STATUS_OK){
+      callback(postRequest.responseText);
+    }
+    else{
+      var JSONPost = postRequest.responseText;
+      var parsedPost = JSON.parse(JSONPost);
+      callback(null, parsedPost);
+    }
+  });
+  postRequest.open('POST', '/fatJSON');
+  postRequest.setRequestHeader('Content-type', 'application/json');
+  postRequest.send(JSON.stringify(post));   
+}
 
-  ChampionModel.getChamp = function(callback, itemID){
-    var getRequest = new XMLHttpsRequest();
-    getRequest.addEventListener('load', function(event){
-      if (getRequest.status != STATUS_OK){
-        callback(getRequest.responseText);
-      }
-      else{
-        var JSONPosts = getRequest.responseText;
-        var parsedPosts = JSON.parse(JSONPosts);
-        return callback(null, parsedPosts);
-      }
-    });
-    getRequest.open('GET', ITEM_URL_1 + itemID + ITEM_URL_2);
-    getRequest.send();
-  }
-
-  PostModel.postFatJSON = function(post, callback) {
-    var postRequest = new XMLHttpRequest();
-    postRequest.addEventListener('load', function(event){
-      if (postRequest.status != STATUS_OK){
-        callback(postRequest.responseText);
-      }
-      else{
-        var JSONPost = postRequest.responseText;
-        var parsedPost = JSON.parse(JSONPost);
-        callback(null, parsedPost);
-      }
-    });
-    postRequest.open('POST', '/fatJSON');
-    postRequest.setRequestHeader('Content-type', 'application/json');
-    postRequest.send(JSON.stringify(post));   
-  };
-
-
-  window.ChampionModel = ChampionModel;
-})(this, this.document);
