1ab0d440b1d4f9ac1e08a8d8540e685d80274979
aaronbrackett472 <aaronbrackett472@gmail.com>
2015-08-26 23:54:55 -0400

    THE SECOND DUMBEST CHANGE
    youll never know

--- champion-model.js
+++ champion-model.js
@@ -1,6 +1,6 @@
 var api_key = 'b9f2effe-c90e-4633-b36b-5716684ccbac' //should these lines have semi-colons? -tcj
 var FREE_CHAMPS_URL= 'https://na.api.pvp.net/api/lol/na/v1.2/champion?freeToPlay=true&api_key=' + api_key //should these lines have semi-colons? -tcj
-var CHALLENGER_URL = 'https://na.api.pvp.net/api/lol/na/v2.5/league/challenger?type=RANKED_SOLO_5x5&api_key' + api_key //should these lines have semi-colons? -tcj
+var CHALLENGER_URL = 'https://na.api.pvp.net/api/lol/na/v2.5/league/challenger?type=RANKED_SOLO_5x5&api_key=' + api_key //should these lines have semi-colons? -tcj
 var MH_URL_1 = 'https://na.api.pvp.net/api/lol/na/v2.2/matchhistory/'
 var MH_URL_2 = '?championIds=' //should these lines have semi-colons? -tcj
 var MH_URL_3 = '&api_key=' + api_key //should these lines have semi-colons? -tcj
