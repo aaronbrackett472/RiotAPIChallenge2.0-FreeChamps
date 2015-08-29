a53e6bc1d0b60e139f1e81260400b2c2a3fb8598
aaronbrackett472 <aaronbrackett472@gmail.com>
2015-08-29 14:09:11 -0400

    Debug

--- items.js
+++ items.js
@@ -17,6 +17,7 @@ function getFatJSON(champs, champItems){
 		}
 		for (var i = 0; i < challengers.entries.length; i++){
 			var challenger = challengers.entries[0]
+			console.log ("Challenger: " + challenger.id)
 			setTimeout(function(challenger, champs, champItems){
 				ChampionModel.matchHistory(function(error, history, champs, champItems){
 					if (error){
